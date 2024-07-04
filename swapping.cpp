60cf5cee1cbb76cc5167e8b59ad74bd4af83eb84
Kanav <kanavmahj@gmail.com>
2024-07-04 11:20:19 +0530

    Update swapping.cpp

--- swapping.cpp
+++ swapping.cpp
@@ -8,9 +8,26 @@ int main(){
 	cout<<"Enter both numbers: ";
 	cin>>a>>b;
 	temp = a;
-    a = b;
-    b = temp;
+    	a = b;
+    	b = temp;
 	cout<<"Values of a and b: "<<endl<<a<<b;
 	return 0;
 	}
+
+/***wap to swap two variables
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a, b, temp;
+	cout<<"Enter both numbers: ";
+	cin>>a>>b;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"Values of a and b: "<<endl<<a<<b;
+	return 0;
+	}***/
+	
 	
