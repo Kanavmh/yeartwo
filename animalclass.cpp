bea7e243037adf930149afd83698ba437ef981e3
Kanav <kanavmahj@gmail.com>
2024-07-08 12:40:18 +0530

    Create animalclass.cpp

---
+++ animalclass.cpp
@@ -0,0 +1,25 @@
+//wap for animals and functions
+#include <iostream>
+using namespace std;
+class animal{
+    public:
+    void eat(){
+        cout<<"Animal eats"<<endl;
+        
+    }
+    void sleep(){
+        cout<<"Animal sleeps"<<endl;
+    }
+};
+class dog{
+    public:
+    void bark(){
+        cout<<"Dog barks"<<endl;
+        }
+    };
+int main() {
+    dog mydog;
+    mydog.bark();
+    return 0;
+    
+}
