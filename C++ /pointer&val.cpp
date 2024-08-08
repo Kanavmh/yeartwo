c135edcf28df1f20385425c7d1196f280ad0a5cf
Kanav <kanavmahj@gmail.com>
2024-08-08 11:39:52 +0530

    Update pointer&val.cpp

--- C++ /pointer&val.cpp
+++ C++ /pointer&val.cpp
@@ -7,28 +7,33 @@ int main(){
     float * f = new float;
     double * d= new double;
     long * l = new long;
+    bool * b = new bool;
     
     cout<<a<<endl;
     cout<<c<<endl;
     cout<<f<<endl;
     cout<<d<<endl;
     cout<<l<<endl;
+    cout<<b<<endl;
     
-    cout<<*a;
-    cout<<*c;
-    cout<<*f;
-    cout<<*d;
-    cout<<*l;
+    cout<<*a<<endl;
+    cout<<*c<<endl;
+    cout<<*f<<endl;
+    cout<<*d<<endl;
+    cout<<*l<<endl;
+    cout<<*b<<endl;
     
-    cout<<sizeof(a);
-    cout<<sizeof(c);
-    cout<<sizeof(f);
-    cout<<sizeof(d);
-    cout<<sizeof(l);
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(l)<<endl;
+    cout<<sizeof(b)<<endl;
     
     delete a;
     delete c;
     delete f;
     delete d;
     delete l;
+    delete b;
 }
