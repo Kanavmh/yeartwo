62b92fb250b7df856f0c52b41fd0c9b40a64b2a4
Kanav <kanavmahj@gmail.com>
2024-12-27 11:45:07 +0530

    Create Date.java

---
+++ java/Date.java
@@ -0,0 +1,40 @@
+public class Date {
+    public static void main(String[] args){
+        // 23.3.04
+        // 15/4/1998
+        // 12-3-2004
+        String date1 = "23.3.04";
+        String date2 = "15/4/1998";
+        String date3 = "12-3-2004";
+        System.out.println(format(date1));
+        System.out.println(format(date2));
+        System.out.println(format(date3));
+
+    }
+    
+    public static int format(String date){
+
+        String[] dateEl = date.split("[./-]");
+        String first = dateEl[0];
+        String second = dateEl[1];
+        String third = dateEl[2];
+
+        if(first.length() ==1){
+            first = "0" + first;
+        }
+        if(second.length()==1){
+            second = "0" + second;
+        }       
+        if(third.length() == 2){
+            if(Integer.parseInt(third) > 20){
+                third = "19" + third;
+            } else{
+                third = "20" + third;
+            }
+        }
+
+        int finalval = Integer.parseInt(first + second + third);
+        return finalval;
+    }
+    
+}
